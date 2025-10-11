064504d94515617cbe5a56952435972aa2fdcd33
Vlados743 <77863403+Vlados743@users.noreply.github.com>
2025-10-11 10:54:14 +0000

    Лаба 3

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
+    char temp;
+    while (left < right) {
+        temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        left++;
+        right--;
+    }
 }
+
 
